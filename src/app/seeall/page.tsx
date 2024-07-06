88bf7b2fe63ebe51349a3373fef419b3171319a4
Didier Nsengiyumva <nsedidier@gmail.com>
2024-07-06 15:55:27 +0200

    4 days to go progress - client

--- src/app/seeall/page.tsx
+++ src/app/seeall/page.tsx
@@ -4,12 +4,24 @@ import Link from 'next/link';
 
 // ListTile component for individual products
 const ListTile = ({ product }: any) => {
+    //var num = product.price;
+    function commafy(num : any) {
+        var str = num.toString().split('.');
+        if (str[0].length >= 5) {
+            str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+        }
+        if (str[1] && str[1].length >= 5) {
+            str[1] = str[1].replace(/(\d{3})/g, '$1 ');
+        }
+        return str.join('.');
+    }
+
   return (
     <div className="border rounded-lg p-4 mb-4 flex items-center hover:-translate-y-1 cursor-pointer">
       <img src={product.image} alt={product.name} className="w-24 h-24 object-cover mr-4" />
       <div>
         <h3 className="text-lg font-semibold">{product.name}</h3>
-        <p className="text-gray-600">${product.price.toFixed(2)}</p>
+        <p className="text-gray-600">RWF{commafy(product.price.toFixed(2))}</p>
         <p className="text-sm text-gray-500">{product.description}</p>
       </div>
     </div>
@@ -20,9 +32,9 @@ const ListTile = ({ product }: any) => {
 export default function Seeall() {
   // Sample product data
   const products = [
-    { id: 1, name: "Product 1", price: 19.99, description: "Description for Product 1", image: "https://via.placeholder.com/150" },
-    { id: 2, name: "Product 2", price: 29.99, description: "Description for Product 2", image: "https://via.placeholder.com/150" },
-    { id: 3, name: "Product 3", price: 39.99, description: "Description for Product 3", image: "https://via.placeholder.com/150" },
+    { id: 1, name: "Apple iTouch", price: 19990, description: "Description for Product 1", image: "https://res.cloudinary.com/dezvucnpl/image/upload/v1720272875/katlyn-luz-ZreKkcuqMU8-unsplash_vaxrq2.jpg" },
+    { id: 2, name: "2.4G Wireless Keyboard", price: 29000, description: "Description for Product 2", image: "https://res.cloudinary.com/dezvucnpl/image/upload/v1720272880/bryan-natanael-hR8l1s4u8QE-unsplash_on46hy.jpg" },
+    { id: 3, name: "USB C-cable", price: 4000, description: "Description for Product 3", image: "https://res.cloudinary.com/dezvucnpl/image/upload/v1720272910/maxence-pira-OJJhibC7cEM-unsplash_gg3q0n.jpg" },
   ];
 
   // Sample categories for sidebar
@@ -46,7 +58,7 @@ export default function Seeall() {
       </div>
 
       {/* Product list */}
-      <div className='w-3/4 p-4'>
+      <div className='w-full p-4 sm:w-3/4'>
         <h1 className="text-2xl font-bold mb-6">All Products</h1>
         {products.map(product => (
           <ListTile key={product.id} product={product} />
